ea8110d28ed255798e61030e28d33dbfca1825d7
mizan2832 <programmer620@gmail.com>
2021-05-06 08:52:11 +0600

    search, style

--- resources/views/front/include/head.blade.php
+++ resources/views/front/include/head.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
     <meta name="author" content="themefisher.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>We are here to give you a good tuitor for your children.</title>
 
--- resources/views/front/tuition_job.blade.php
+++ resources/views/front/tuition_job.blade.php
@@ -9,42 +9,81 @@
                     <small>Job</small>
                 </h1>
                 <!-- Blog post-->
-                <div class="card mb-4">
-                    
+                <div class="card" style="color: black">
+                    <div class="card-header">Need a Female Tuitor for A level Student</div>
                     <div class="card-body">
-                        <h3 class="card-title">Need a Female Tuitor for A level Student</h3>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                        <a class="btn btn-primary" href="#!">Apply →</a>
+                      
+                         <div class="row col-md-12 m-0">
+                             <div class="col-md-4">Category:Male</div>
+                             <div class="col-md-4" style="margin-left: -20px;">Class:</div>
+                             <div class="col-md-4 text-center">Salary:1241</div>
+                         </div>
+                         <p>Subjects:Bangla,Math,Physics</p>
+                         <p>5days</p>
+                         <p>Requirments</p>
+                        
+                    </div>
+                    <div class="card-footer text-muted">
+                        Posted on January 1, 2021 
+                    </div>
+                </div>
+                <div class="card mb-30">
+                    <div class="card-header">Need a Female Tuitor for A level Student</div>
+                    <div class="card-body">
+                      
+                         <div class="row col-md-12 m-0">
+                             <div class="col-md-4">Category:Male</div>
+                             <div class="col-md-4" style="margin-left: -20px;">Class:</div>
+                             <div class="col-md-4 text-center">Salary:1241</div>
+                         </div>
+                         <p>Subjects:Bangla,Math,Physics</p>
+                         <p>5days</p>
+                         <p>Requirments</p>
+                        
+                    </div>
+                    <div class="card-footer text-muted">
+                        Posted on January 1, 2021 
+                    </div>
+                </div>
+
+                <div class="card ">
+                    <div class="card-header">Need a Female Tuitor for A level Student</div>
+                    <div class="card-body">
+                      
+                         <div class="row col-md-12 m-0">
+                             <div class="col-md-4">Category:Male</div>
+                             <div class="col-md-4" style="margin-left: -20px;">Class:</div>
+                             <div class="col-md-4 text-center">Salary:1241</div>
+                         </div>
+                         <p>Subjects:Bangla,Math,Physics</p>
+                         <p>5days</p>
+                         <p>Requirments</p>
+                        
+                    </div>
+                    <div class="card-footer text-muted">
+                        Posted on January 1, 2021 
+                    </div>
+                </div>
+                <div class="card ">
+                    <div class="card-header">Need a Female Tuitor for A level Student</div>
+                    <div class="card-body">
+                      
+                         <div class="row col-md-12 m-0">
+                             <div class="col-md-4">Category:Male</div>
+                             <div class="col-md-4" style="margin-left: -20px;">Class:</div>
+                             <div class="col-md-4 text-center">Salary:1241</div>
+                         </div>
+                         <p>Subjects:Bangla,Math,Physics</p>
+                         <p>5days</p>
+                         <p>Requirments</p>
+                        
                     </div>
                     <div class="card-footer text-muted">
                         Posted on January 1, 2021 
                     </div>
                 </div>
                 <!-- Blog post-->
-                <div class="card mb-4">
-                    
-                    <div class="card-body">
-                        <h2 class="card-title">Post Title</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                        <a class="btn btn-primary" href="#!">Apply →</a>
-                    </div>
-                    <div class="card-footer text-muted">
-                        Posted on January 1, 2021 
-        
-                    </div>
-                </div>
-                <!-- Blog post-->
-                <div class="card mb-4">
-                    
-                    <div class="card-body">
-                        <h2 class="card-title">Post Title</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                        <a class="btn btn-primary" href="#!">Apply →</a>
-                    </div>
-                    <div class="card-footer text-muted">
-                        Posted on January 1, 2021 
-                    </div>
-                </div>
+               
                 <!-- Pagination-->
                 <ul class="pagination justify-content-center mb-4">
                     <li class="page-item"><a class="page-link" href="#!">← Older</a></li>
@@ -53,41 +92,89 @@
             </div>
             <!-- Side widgets-->
             <div class="col-md-4">
-                <!-- Search widget-->
-                <div class="card my-4">
-                    <h5 class="card-header">Search</h5>
-                    <div class="card-body">
-                        <div class="input-group">
-                            <input class="form-control" type="text" placeholder="Search for..." />
-                            <span class="input-group-append"><button class="btn btn-secondary" type="button">Go!</button></span>
-                        </div>
-                    </div>
-                </div>
+           
                 <!-- Categories widget-->
                 <div class="card my-4">
-                    <h5 class="card-header">Categories</h5>
+                    <h5 class="card-header">Search Tuition</h5>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-lg-6">
-                                <ul class="list-unstyled mb-0">
-                                    <li><a href="#!">Web Design</a></li>
-                                    <li><a href="#!">HTML</a></li>
-                                    <li><a href="#!">Freebies</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-lg-6">
-                                <ul class="list-unstyled mb-0">
-                                    <li><a href="#!">JavaScript</a></li>
-                                    <li><a href="#!">CSS</a></li>
-                                    <li><a href="#!">Tutorials</a></li>
-                                </ul>
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <select class="form-control" id="sel1">
+                                      <option>Select District</option>
+                                      <option>Dhaka</option>
+                                      <option>Tangail</option>
+                                      <option>Chattagong</option>
+                                    </select>
+                                  </div>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <select class="form-control" id="sel1">
+                                      <option>Select Area</option>
+                                      <option>Dhaka</option>
+                                      <option>Tangail</option>
+                                      <option>Chattagong</option>
+                                    </select>
+                                  </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <select class="form-control" id="sel1">
+                                      <option>Select Medium</option>
+                                      <option>Dhaka</option>
+                                      <option>Tangail</option>
+                                      <option>Chattagong</option>
+                                    </select>
+                                  </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <select class="form-control" id="sel1">
+                                      <option>Select Class</option>
+                                      <option>Dhaka</option>
+                                      <option>Tangail</option>
+                                      <option>Chattagong</option>
+                                    </select>
+                                  </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <select class="form-control" id="sel1">
+                                      <option>Select Subject</option>
+                                      <option>Dhaka</option>
+                                      <option>Tangail</option>
+                                      <option>Chattagong</option>
+                                    </select>
+                                  </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <select class="form-control" id="sel1">
+                                      <option>Select Gender</option>
+                                      <option>Dhaka</option>
+                                      <option>Tangail</option>
+                                      <option>Chattagong</option>
+                                    </select>
+                                  </div>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
                 <!-- Side widget-->
                 <div class="card my-4">
-                    <h5 class="card-header">Side Widget</h5>
+                    <h5 class="card-header">Category</h5>
                     <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!</div>
                 </div>
             </div>
