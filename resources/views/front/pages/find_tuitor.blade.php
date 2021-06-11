63168a75117180466893a118c13e91b171379d59
mizan2832 <programmer620@gmail.com>
2021-06-11 11:16:10 +0600

    find tuitor form

--- resources/views/front/pages/find_tuitor.blade.php
+++ resources/views/front/pages/find_tuitor.blade.php
@@ -14,24 +14,63 @@
         </div>
            <div id="tuitor_form">
                 <h3 style="color: blue; border-bottom:2px solid black; text-align:center;padding:3px;">Request a tuitor by filling out the form.</h3>
-                <div class="main-card mb-3 card">
-                    <div class="card-body">
-                        <h5 class="card-title">Grid Rows</h5>
+            
                         <form class="">
-                            <div class="form-row">
+                            <div class="row col-md-12" style="margin-left: -10px;">
                                 <div class="col-md-6">
-                                    <div class="position-relative form-group"><label for="exampleEmail11" class="">Email</label><input name="email" id="exampleEmail11" placeholder="with a placeholder" type="email" class="form-control"></div>
+                                    <div class="form-group"><label for="studentName" class="">Student Name:</label><input name="studentName" id="" placeholder="Enter the student name" type="text" class="form-control"></div>
                                 </div>
                                 <div class="col-md-6">
-                                    <div class="position-relative form-group"><label for="examplePassword11" class="">Password</label><input name="password" id="examplePassword11" placeholder="password placeholder" type="password" class="form-control">
+                                    <div class="form-group"><label for=" location" class="">Location</label><input name="location" id=" location" placeholder="Enter your location" type="password" class="form-control">
                                     </div>
                                 </div>
                             </div>
-                        
-                     </div>
-                </div>
-            </div>
+                            <label for="gender" style="margin-left:15px;" class="">Gender:</label>
+                            <div class="form-row" style="margin-left:13px;">
+                                <div class="col-md-6">
+                                    <div class="position-relative form-group"><input type="radio" id="male" name="gender" value="male">   Male</div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="position-relative form-group">
+                                        <input type="radio" id="female" name="gender" value="female">   Female
+                                </div>
+                            </div>
+                            <label for="subject" class="" style="margin-left:5px;">Subject for tuitoring:</label>
+                            <div class="col-md-6">
+                                <select name="subject" id="subject">
+                                    <option value="m">Math</option>
+                                    <option value="e">English</option>
+                                    <option value="p">Physics</option>
+                                    <option value="c">Chemistry</option>
+                                </select>
+                            </div>     
+                            <div class="row col-md-12">
+                                <div class="col-md-6">
+                                    <div class="position-relative form-group"><label for="phoneNumber" class="">Phone Number:</label><input name="phoneNumber" id="" placeholder="Enter the phone number" type="text" class="form-control"></div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="position-relative form-group"><label for=" location" class="">How much salary you want to give? </label><input name="salary" id=" salary" placeholder="Enter your salary" type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row col-md-12">
+                                <div class="col-md-6">
+                                    <div class="position-relative form-group"><label for="tuitoringDays" class="">Tuitoring days in a week:</label><input name="tuitoringDays" id="" placeholder="Number of days in a week" type="text" class="form-control"></div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="position-relative form-group"><label for="tutoringType" class="">Type of tuitoring? </label> <br>
+                                    <select name="tutoringType" id="tutoringType">
+                                            <option value="offline">Offline</option>
+                                            <option value="online">Online</option>
+                                    </select>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                               <input type="button"  class="button btn-success btn" value="submit">
+                           
+                    </div>
      
-    </div>
+             </div>
   </div>
 @endsection
