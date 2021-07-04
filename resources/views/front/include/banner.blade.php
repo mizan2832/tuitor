cec8fa6e758ccd90678630ba214a6d0e18d2f772
mizan2832 <programmer620@gmail.com>
2021-07-04 20:00:17 +0600

    password reset

--- resources/views/front/include/banner.blade.php
+++ resources/views/front/include/banner.blade.php
@@ -4,9 +4,9 @@
             <div class="col-lg-6 col-md-12 col-xl-7">
                 <div class="block">
                     <div class="divider mb-3"></div>
-                    <h1 class="mb-3 mt-3">Your most trusted tuitor provider</h1>
+                    <h1 style="color:#fff;" class="mb-3 mt-3">Your most trusted tuitor provider</h1>
 
-                    <p class="mb-4 pr-5">Master.com is a faster, easier way to find local and online tutoring services.</p>
+                    <p style="color:#fff;" class="mb-4 pr-5">Master.com is a faster, easier way to find local and online tutoring services.</p>
                     <div class="btn-container ">
                         <a href="{{ url('find-tuitor') }}" class="btn btn-main-2 btn-icon btn-round-full">Find Your Tuitor <i class="icofont-simple-right ml-2  "></i></a>
                     </div>
